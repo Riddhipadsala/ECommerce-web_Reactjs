0d4003aa007fc545f2021b0e6ab8c43d0f6bfa19
Riddhipadsala <riddhipadsala52@gmail.com>
2025-06-17 22:16:26 +0530

    Create App.js
    add app js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App_web from './Aapno_project/App_web';
+
+const App=()=>{
+  return(
+      <App_web />
+    );
+
+}
+
+export default App;
